eccf222ee8c9b07e91e2ab272e7ebd08804b5271
Adam M. Jacobs <ajacobs@pa.msu.edu>
2019-08-27 14:48:16 -0400

    increase sleep in db script, too

--- init_db.sh
+++ init_db.sh
@@ -2,6 +2,6 @@
 
 docker-compose up -d db
 wait
-sleep 5 #Hacky way of giving the db enough time to get up and running. 
+sleep 10 #Hacky way of giving the db enough time to get up and running. 
         #`wait` alone isn't sufficient.
 docker-compose run --rm flaskapp /bin/bash -c "cd /opt/services/flaskapp/src && python -c  'import database; database.init_db()'"
