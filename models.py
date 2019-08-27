af118d1abfe925e4355c6bb1686f14bd487eb6b3
Adam M. Jacobs <ajacobs@pa.msu.edu>
2019-08-27 12:15:21 -0400

    Add str dunder to make Items printable

--- models.py
+++ models.py
@@ -12,3 +12,12 @@ class Items(Base):
     quantity = Column(Integer)
     description = Column(String(256))
     date_added = Column(DateTime())
+
+    def __str__(self):
+        """Return basic HTML string that can be rendered for this object."""
+        ret_fmt = "Item for sale: {}</br>"
+        ret_fmt = ret_fmt + "&emsp; Description: {}</br>"
+        ret_fmt = ret_fmt + "&emsp; Quantity available: {}</br>"
+        ret_fmt = ret_fmt + "&emsp; Date listed: {}</br></br>"
+        return ret_fmt.format(self.name, self.description, self.quantity,
+                              self.date_added)
