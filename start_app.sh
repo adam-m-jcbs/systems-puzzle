ebf5d0e76c08dcc4bf487986d0b3ca5fc0906e82
Adam M. Jacobs <ajacobs@pa.msu.edu>
2019-08-27 14:45:11 -0400

    make hacky sleep times a bit longer in convenience script

--- start_app.sh
+++ start_app.sh
@@ -2,8 +2,8 @@
 
 docker-compose up -d db
 wait
-sleep 5
+sleep 10
 docker-compose run --rm flaskapp /bin/bash -c "cd /opt/services/flaskapp/src && python -c  'import database; database.init_db()'"
 wait
-sleep 5
+sleep 10
 docker-compose up -d
